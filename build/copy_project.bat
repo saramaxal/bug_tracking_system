849288af64be8cba7ea50acf676475702c876f98
saramaxal <saramaxal@gmail.com>
2023-10-02 21:11:57 +0300

    readme

--- build/copy_project.bat
+++ build/copy_project.bat
@@ -1,9 +1,10 @@
 @echo off
 
+SET COMPILER="E:\Programs\bin\cfencode.exe"
+
 SET PH=%~dp0..
 SET BLD=%PH%\build
 SET PRJ=%BLD%\link-to-project
-SET COMPILER="E:\Programs\bin\cfencode.exe"
 
 del /s /q %PRJ%\*
 call %COMPILER% %PH%\*.html %PRJ%\*.cfm /v 2
